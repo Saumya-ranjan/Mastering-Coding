b17364117a9598a190f246c1b4fd467ed3191738
Saumyaranjan <saumyaranjan.sp20@gmail.com>
2022-01-21 11:24:56 +0530

    ahash tables

--- Dictionaries_Hash/recurring.py
+++ Dictionaries_Hash/recurring.py
@@ -1,18 +1,21 @@
 # def func(x):
+#   list = []
 #   for i in range(0,len(x)):
 #     for j in range(i+1,len(x)):
 #       if x[i]==x[j]:
-#         print(x[i])
+#         list.append(x[i])
+#   print(list)
+      
         
 
-from tkinter import X
 
 
 def hashtable(x):                     # O(n) rather than O(n^2)
-  mydict = {}
+  mydict = {}                         #so it becomes time efficient
   for i in range(0,len(x)):          
     if x[i] in mydict:
-      return X[i]
+      print(x[i])
+      break
     else:
       mydict[x[i]]=i
   return 0
@@ -21,4 +24,4 @@ def hashtable(x):                     # O(n) rather than O(n^2)
 mylist = [2,1,1,2,3,4,5,3]
 x = hashtable(mylist)
 # func(mylist)
-print(x)
+# print(x)
