9f83e4364bea1000ba87ac5de893770d2912338f
Saumyaranjan <saumyaranjan.sp20@gmail.com>
2022-01-25 16:48:52 +0530

    Starting eith patterns in python

--- Algorithms/Recursion/fibonnacci_recursive.py
+++ Algorithms/Recursion/fibonnacci_recursive.py
@@ -1,27 +1,31 @@
-# BASIC METHOD:       It's My Method
+# BASIC METHOD: Iterative one       It's My Method
 
-# def func(x):                               
-#     arr = [1,1]
-#     a=1
-#     b=1
-#     for i in range(x-2):
-#         c = a+b
-#         a=b
-#         b = c
+#Anything can be done recursively can be done iteratively(loop) too.
+
+def func(x):                            #O(n)                 
+    arr = [1,1]
+    a=1
+    b=1
+    for i in range(x-2):
+        c = a+b
+        a=b
+        b = c
         
-#         arr .append(c)
-#     print(arr)
-# func(int(input("Enter a Number: ")))
+        arr .append(c)
+    print(arr)
+func(int(input("Enter a Number: ")))
 
 
-# RECURSIVE METHOD:  
+# RECURSIVE METHOD:    [This is Slow and Not time efficient]
 
-def func(x):
-    arr = []
-    if x == 1:
-        return 1
-    else:
-        return arr.append(x+func(x-1))
+# [0,1,1,2,3,5,8,13,21]
+
+# def func(x):                                 #o(2^n)
+#     arr = []
+#     if x < 2:
+#         return x
+   
+#     return func(x-1)+func(x-2)
 
 
-print(func(int(input("Enter the Number: "))))
+# print(func(int(input("Enter the Number: "))))
